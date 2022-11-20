e84a6b1c1d8252d87e6fa1f4a3e5f49ea4e48a5d
hhmadan <hhmadan03@gmail.com>
2022-11-20 19:27:04 +0530

    REFACTORED: Created Parameterised Constructor and Pass Test Case giving message in Constructor

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -1,5 +1,11 @@
 package com.moodanalyser;
 public class MoodAnalyser {
+    public String message;
+    public MoodAnalyser(){                                // Default constructor
+    }
+    public MoodAnalyser(String message){                  // Parameterised constructor with parameter message
+        this.message = message;
+    }
     public String analyseMood(String message) {           //analyseMood function with mood as parameter
         if (message.contains("sad")) {
             return "SAD MOOD";
@@ -7,8 +13,17 @@ public class MoodAnalyser {
             return "HAPPY MOOD";
         }
     }
+    public String analyserMood(){                       //support no parameters and use message field defined for the Class
+        if (message.contains("sad")) {
+            return "SAD MOOD";
+        } else {
+            return "HAPPY MOOD";
+        }
+    }
     public static void main(String[] args) {
-        MoodAnalyser mood = new MoodAnalyser();             //MoodAnalyser object
+        MoodAnalyser mood = new MoodAnalyser();                         //MoodAnalyser object, default constructor
         System.out.println(mood.analyseMood("I am in any mood"));
+        MoodAnalyser mood1 = new MoodAnalyser("I am in sad mood");      //MoodAnalyser object, parameterised constructor
+        System.out.println(mood1.analyserMood());
     }
 }
--- src/test/java/com/moodanalysertest/MoodAnalyserTest.java
+++ src/test/java/com/moodanalysertest/MoodAnalyserTest.java
@@ -7,9 +7,17 @@ import static junit.framework.TestCase.assertEquals;
 
 public class MoodAnalyserTest {
     @Test
-    public void test_SadMood(){
+    public void test_Mood(){
         MoodAnalyser mood = new MoodAnalyser();
         assertEquals("SAD MOOD",mood.analyseMood("I am in sad mood"));
         assertEquals("HAPPY MOOD",mood.analyseMood("I am in any mood"));
+    }
+    @Test
+    public void test_MoodUsingConstructor(){
+        MoodAnalyser mood = new MoodAnalyser("I am in Happy Mood");
+        assertEquals("HAPPY MOOD",mood.analyserMood());
+
+        MoodAnalyser mood1 = new MoodAnalyser("I am in sad Mood");
+        assertEquals("SAD MOOD",mood1.analyserMood());
     }
 }
